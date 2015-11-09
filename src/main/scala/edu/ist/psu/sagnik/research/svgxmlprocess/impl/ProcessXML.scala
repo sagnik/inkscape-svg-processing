29d04e2e716c1909f38d30c8f4a73d964009f135
sagnik <sagnik1987@gmail.com>
2015-11-08 20:45:27 -0500

    working on getting groups for SVGPaths, test failing :(

--- src/main/scala/edu/ist/psu/sagnik/research/svgxmlprocess/impl/ProcessXML.scala
+++ src/main/scala/edu/ist/psu/sagnik/research/svgxmlprocess/impl/ProcessXML.scala
@@ -3,15 +3,24 @@ package edu.ist.psu.sagnik.research.svgxmlprocess.impl
 import edu.ist.psu.sagnik.research.svgxmlprocess.model.{pathOp, SVGPath}
 import edu.ist.psu.sagnik.research.svgxmlprocess.reader.XMLReader
 
+import scala.xml.{Node, NodeSeq}
+
 /**
  * Created by szr163 on 11/8/15.
  */
+
+case class PathGroups(path:Node,gIds:Seq[String])
+
 object ProcessXML {
   def apply(fileLoc:String)=getPaths(XMLReader(fileLoc))
 
   def getPaths(xmlContent:scala.xml.Elem):Seq[SVGPath]= {
-    val topLevelpaths = xmlContent \ "path"
-    topLevelpaths.map(x =>
+
+    val topLevelPaths = xmlContent \ "path"
+    val lowerLevelPaths=iterateOverGroups(xmlContent \ "g",Seq.empty[PathGroups],Seq.empty[String])
+
+    val topLevelSVGPaths=
+      topLevelPaths.map(x =>
       SVGPath(
         x.attribute("id") match {
           case Some(idExists) => idExists.text
@@ -23,6 +32,34 @@ object ProcessXML {
         gIds = Seq.empty[String]
       )
       )
+
+    val lowerLevelSVGpaths=
+    lowerLevelPaths.map(x =>
+      SVGPath(
+        x.path.attribute("id") match {
+          case Some(idExists) => idExists.text
+          case _ => "noID"
+        },
+        pdContent = x.path.attribute("d") match {
+          case Some(con) => con.text
+          case _ => ""
+        },
+        pContent = x.path.toString(),
+        pOps = Seq.empty[pathOp],
+        gIds = x.gIds
+      )
+      )
+    topLevelSVGPaths ++ lowerLevelSVGpaths
   }
+
+  def iterateOverGroups(tlGs:NodeSeq,pathGIDs:Seq[PathGroups],gIdsSofar:Seq[String]):Seq[PathGroups]=
+    if (tlGs.length==0) pathGIDs
+    else{
+      val newGId=tlGs.head \@ "id" //TODO: What happens if the group doesn't have an ID?
+      val thisTLGpathGIDs=(tlGs.head \ "path").map(x=>PathGroups(x,gIdsSofar:+newGId))
+      iterateOverGroups(tlGs.head \"g",pathGIDs++thisTLGpathGIDs,gIdsSofar:+newGId)
+    }
+
+
 
 }
--- src/test/scala/edu/ist/psu/sagnik/research/svgxmlprocess/test/SVGPathOutputTest.scala
+++ src/test/scala/edu/ist/psu/sagnik/research/svgxmlprocess/test/SVGPathOutputTest.scala
@@ -12,7 +12,14 @@ class SVGPathOutputTest extends FunSpec {
   describe("testing the SVG output by printing") {
     import edu.ist.psu.sagnik.research.svgxmlprocess.test.DataLocation._
     it("should print the path info.") {
-      ProcessXML(svgFileLoc).foreach(x=>println(s"[path content]: ${x.pContent}, [path id]: ${x.id}, [path ops]: ${x.pOps}"))
+      val results=ProcessXML(svgFileLoc)
+      results.foreach(x=>println(s"[path content]: ${x.pContent}, [path id]: ${x.id}, [groupIds]: ${x.gIds}"))
+
+      assert(results.find(p=>p.id=="p1").head.gIds.isEmpty)
+      assert(results.find(p=>p.id=="p2").head.gIds.isEmpty)
+      assert(List("g1","g2").toIndexedSeq.equals(results.find(p=>p.id=="p3").head.gIds))
+      assert(List("g1").toIndexedSeq.equals(results.find(p=>p.id=="p4").head.gIds))
+      assert(List("g3").toIndexedSeq.equals(results.find(p=>p.id=="p5").head.gIds))
     }
   }
 
