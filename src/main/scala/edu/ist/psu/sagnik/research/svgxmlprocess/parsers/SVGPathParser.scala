b5c6b4c5288a0a78fb66d12c53996f4abf9b9ee5
sagnik <sagnik1987@gmail.com>
2015-11-12 18:33:29 -0500

    adding stuff for SVGParser, testing, WIP

--- src/main/scala/edu/ist/psu/sagnik/research/svgxmlprocess/parsers/SVGPathParser.scala
+++ src/main/scala/edu/ist/psu/sagnik/research/svgxmlprocess/parsers/SVGPathParser.scala
@@ -7,14 +7,21 @@ import scala.util.parsing.combinator.RegexParsers
  */
 
 class SVGPathParser extends RegexParsers {
+
   def wsp: Parser[String]= """ """.r ^^ {_.toString}
+
+  def comma: Parser[String] = """,""".r ^^ {_.toString}
+
   def digit:Parser[String]="""0|1|2|3|4|5|6|7|8|9""".r ^^ {_.toString}
+
   def digit_sequence: Parser[String]=rep(digit) ^^{_.mkString("")}
+
   def sign:Parser[String]="""\+|\-""".r ^^{_.toString}
-  def exponent:Parser[String]="""e|E""".r~opt(sign)~digit_sequence ^^ { //TODO: Not handling cases such as e!234, which are definitely wrong
-    case exponent~Some(sign)~digitSeq =>
-      if ("-".equals(sign)) scala.math.exp(-1*digitSeq.toDouble).toString else scala.math.exp(digitSeq.toDouble).toString
-    case _ => ""
+
+  def exponent:Parser[Double]="""e|E""".r~opt(sign)~digit_sequence ^^ { //TODO: Not handling cases such as e!234, which are definitely wrong
+    case ep~Some(sign)~digitSeq =>
+      if ("-".equals(sign)) scala.math.exp(-1*digitSeq.toDouble) else scala.math.exp(digitSeq.toDouble)
+    case _ => 1d
   }
 
   def fractional_constant:Parser[String]=
@@ -27,29 +34,45 @@ class SVGPathParser extends RegexParsers {
         case ds~dot=>ds.toString
         case _ => ""
       }
-  //TODO: Why is this ever used?
+
   def floating_point_constant: Parser[String]=
     fractional_constant~opt(exponent)^^{
-      case fc~Some(ep)=>fc+ep
+      case fc~Some(ep)=>(fc.toDouble*ep).toString
+      case fc~None=>fc
     } |
-  digit_sequence~exponent ^^{
-    case ds~ep=>ds+ep
-  }
+      digit_sequence~exponent ^^{
+        case ds~ep=>(ds.toDouble*ep).toString
+      }
+
+  def integer_constant:Parser[String]= digit_sequence^^{_.toString}
+
+  def number:Parser[Double]=
+    opt(sign)~floating_point_constant^^{
+      case Some(s)~fc => if ("-".equals(s)) ("-"+fc).toDouble else fc.toDouble
+      case None~fc => fc.toDouble
+    } |
+      opt(sign)~integer_constant^^{
+        case Some(s)~ic => if ("-".equals(s)) ("-"+ic).toDouble else ic.toDouble
+        case None~ic => ic.toDouble
+      }
+
+  def nonnegative_number:Parser[Double]=
+    floating_point_constant^^{_.toDouble}|integer_constant^^{_.toDouble}
 
 
   /*
-  def digit_sequence_rec: Parser[Option[String]]=digit|digit~digit_sequence_rec ^^ {
-    case Some(oneDigit) | None => Some(oneDigit.toString)
-    case Some(digit1)|Some(digit2)~None => Some(digit1.toString+digit2.toString)
-    case Some(digit1)|Some(digit2)~Some(digitSeq) => Some(digit1.toString+digit2.toString+digitSeq.toString)
-  }
+  def nonnegative_number: Parser[String]=
+    integer-constant
+  | floating-point-constant
+  number:
+    sign? integer-constant
+  | sign? floating-point-constant
   */
-
 }
 
 object TestSVGPathParser extends SVGPathParser{
   def main(args: Array[String]) = {
-    parse(fractional_constant, "1.234") match {
+    parse(floating_point_constant, "12.3e-2") match {
       case Success(matched,_) => println(matched)
       case Failure(msg,_) => println("FAILURE: " + msg)
       case Error(msg,_) => println("ERROR: " + msg)
