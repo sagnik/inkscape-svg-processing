4c2cf1b6c0b258489097e4bfa9520a56b7abe6a7
sagnik <sagnik1987@gmail.com>
2016-01-19 22:12:57 -0500

    modified for older inkscape SVGs

--- src/test/scala/edu/ist/psu/sagnik/research/inkscapesvgprocessing/test/DataLocation.scala
+++ src/test/scala/edu/ist/psu/sagnik/research/inkscapesvgprocessing/test/DataLocation.scala
@@ -4,5 +4,5 @@ package edu.ist.psu.sagnik.research.inkscapesvgprocessing.test
  * Created by szr163 on 11/8/15.
  */
 object DataLocation {
-  val svgFileLoc="src/test/resources/page_006_cortina.svg"
+  val svgFileLoc= "/home/sagnik/jartest/10.1.1.304.6859/page_006.svg"//"src/test/resources/page_006_cortina.svg"
 }
