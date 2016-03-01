dc2ba4fc045a38251e0557d79b960ee94719b306
sagnik <sagnik1987@gmail.com>
2016-03-01 15:49:38 -0500

    JSON production WIP

---
+++ src/main/scala/edu/ist/psu/sagnik/research/inkscapesvgprocessing/writer/impl/JSONWriter.scala
@@ -0,0 +1,16 @@
+package edu.ist.psu.sagnik.research.inkscapesvgprocessing.writer.impl
+
+import edu.ist.psu.sagnik.research.inkscapesvgprocessing.model.SVGPath
+import edu.ist.psu.sagnik.research.inkscapesvgprocessing.textparser.model.SVGChar
+
+/**
+ * Created by szr163 on 3/1/16.
+ */
+object JSONWriter {
+  def apply(jsonLoc:String,svgPaths:Option[Seq[SVGPath]],svgChars:Option[Seq[SVGChar]]): Unit={
+    svgPaths match{
+      case Some(svgPaths)
+    }
+  }
+
+}
---
+++ src/main/scala/edu/ist/psu/sagnik/research/inkscapesvgprocessing/writer/model/JSONDataModel.scala
@@ -0,0 +1,8 @@
+package edu.ist.psu.sagnik.research.inkscapesvgprocessing.writer.model
+
+import edu.ist.psu.sagnik.research.inkscapesvgprocessing.model.Rectangle
+
+/**
+ * Created by szr163 on 3/1/16.
+ */
+case class SVGPathJson(bb:Rectangle,pathString=)
