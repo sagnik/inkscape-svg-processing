2b99ae38307e819b917ffee4621cf90c41880585
sagnik <sagnik1987@gmail.com>
2016-03-02 21:56:52 -0500

    produce JSON wip

--- src/main/scala/edu/ist/psu/sagnik/research/inkscapesvgprocessing/writer/model/JSONDataModel.scala
+++ src/main/scala/edu/ist/psu/sagnik/research/inkscapesvgprocessing/writer/model/JSONDataModel.scala
@@ -5,4 +5,26 @@ import edu.ist.psu.sagnik.research.inkscapesvgprocessing.model.Rectangle
 /**
  * Created by szr163 on 3/1/16.
  */
-case class SVGPathJson(bb:Rectangle,pathString=)
+
+case class PathStyle(fill:String,
+                     stroke:String,
+                     strokeWidth:Float,
+                     strokeLinecap:String,
+                     strokeLinejoin:String,
+                     strokeMiterlimit:String,
+                     strokeDasharray:String,
+                     strokeOpacity:Float)
+
+case class CharStyle(fontVariant:String,
+                     fontWeight:String,
+                     fontSize:Float,
+                     fontFamily:String,
+                     inkscapeFontSpecification:String,
+                     writingMode:String,
+                     fill:String,
+                     fillOpacity:Float,
+                     fillRule:String,
+                     stroke:String)
+
+case class SVGPathJson(bb:Rectangle,pathDString:String,pathStyle:PathStyle,pathWholeString:String)
+case class SVGCharJson(bb:Rectangle,c:Char,charStyle:CharStyle,charWholeString:String)
